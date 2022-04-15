3ac169285bd1e8d04d834d4954f914e4ff9d28ce
pubcontrib <pub.contrib@protonmail.com>
2022-04-15 18:40:50 -0500

    Moved main application functions after the main function.

--- src/main.cpp
+++ src/main.cpp
@@ -13,49 +13,17 @@
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 328;
-
-void applicationLoop(SDL_Window *window, SDL_Renderer *renderer, int argc, char **argv);
-
-std::string readFile(std::string path)
-{
-    std::ifstream stream(path);
-
-    if (stream.is_open())
-    {
-        return std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-    }
-    else
-    {
-        return "`Unable to load file.`";
-    }
-}
-
-bool writeFile(std::string path, std::string text)
-{
-    std::ofstream stream(path);
-
-    if (stream.is_open())
-    {
-        stream << text;
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-#define SYMBOL_ESCAPE '\\'
-#define SYMBOL_COMMENT '`'
-#define SYMBOL_NUMBER '#'
-#define SYMBOL_STRING '"'
-#define SYMBOL_LIST_START '['
-#define SYMBOL_LIST_END ']'
-#define SYMBOL_MAP_START '{'
-#define SYMBOL_MAP_END '}'
-#define SYMBOL_CALL_START '('
-#define SYMBOL_CALL_END ')'
-#define SYMBOL_NULL '?'
+const char SYMBOL_ESCAPE = '\\';
+const char SYMBOL_COMMENT = '`';
+const char SYMBOL_NUMBER = '#';
+const char SYMBOL_STRING = '"';
+const char SYMBOL_LIST_START = '[';
+const char SYMBOL_LIST_END = ']';
+const char SYMBOL_MAP_START = '{';
+const char SYMBOL_MAP_END = '}';
+const char SYMBOL_CALL_START = '(';
+const char SYMBOL_CALL_END = ')';
+const char SYMBOL_NULL = '?';
 
 typedef struct
 {
@@ -70,152 +38,10 @@ typedef struct
     SDL_Color undefined;
 } color_theme_t;
 
-void draw(std::vector<std::string> lines, SDL_Point to, SDL_Point cursor, SDL_Point bounds, color_theme_t *theme, FontSheet *font, SDL_Renderer *renderer)
-{
-    SDL_Point position = {0, 0};
-    SDL_Point min = {(cursor.x / bounds.x) * bounds.x, (cursor.y / bounds.y) * bounds.y};
-    SDL_Point max = {min.x + bounds.x, min.y + bounds.y};
-    auto escaping = false;
-    enum stream_state_t {
-        NOTHING_STREAM_STATE,
-        COMMENT_STREAM_STATE,
-        NUMBER_STREAM_STATE,
-        STRING_STREAM_STATE
-    };
-    auto state = NOTHING_STREAM_STATE;
-
-    for (auto line : lines)
-    {
-        for (auto symbol : line)
-        {
-            if (escaping)
-            {
-                escaping = false;
-            }
-            else
-            {
-                switch (symbol)
-                {
-                    case SYMBOL_ESCAPE:
-                        escaping = true;
-                        break;
-                    case SYMBOL_COMMENT:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            state = COMMENT_STREAM_STATE;
-                        }
-                        else if (state == COMMENT_STREAM_STATE)
-                        {
-                            state = NOTHING_STREAM_STATE;
-                        }
-
-                        break;
-                    case SYMBOL_NUMBER:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            state = NUMBER_STREAM_STATE;
-                        }
-                        else if (state == NUMBER_STREAM_STATE)
-                        {
-                            state = NOTHING_STREAM_STATE;
-                        }
-
-                        break;
-                    case SYMBOL_STRING:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            state = STRING_STREAM_STATE;
-                        }
-                        else if (state == STRING_STREAM_STATE)
-                        {
-                            state = NOTHING_STREAM_STATE;
-                        }
-
-                        break;
-                }
-
-                switch (symbol)
-                {
-                    case SYMBOL_ESCAPE:
-                        break;
-                    case SYMBOL_COMMENT:
-                        if (state == COMMENT_STREAM_STATE)
-                        {
-                            font->setColor(&theme->comments);
-                        }
-
-                        break;
-                    case SYMBOL_NUMBER:
-                        if (state == NUMBER_STREAM_STATE)
-                        {
-                            font->setColor(&theme->numbers);
-                        }
-
-                        break;
-                    case SYMBOL_STRING:
-                        if (state == STRING_STREAM_STATE)
-                        {
-                            font->setColor(&theme->strings);
-                        }
-
-                        break;
-                    case SYMBOL_LIST_START:
-                    case SYMBOL_LIST_END:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            font->setColor(&theme->lists);
-                        }
-
-                        break;
-                    case SYMBOL_MAP_START:
-                    case SYMBOL_MAP_END:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            font->setColor(&theme->maps);
-                        }
-
-                        break;
-                    case SYMBOL_CALL_START:
-                    case SYMBOL_CALL_END:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            font->setColor(&theme->calls);
-                        }
-
-                        break;
-                    case SYMBOL_NULL:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            font->setColor(&theme->nulls);
-                        }
-
-                        break;
-                    default:
-                        if (state == NOTHING_STREAM_STATE)
-                        {
-                            font->setColor(&theme->undefined);
-                        }
-
-                        break;
-                }
-            }
-
-            if (position.x >= min.x && position.x < max.x && position.y >= min.y && position.y < max.y)
-            {
-                auto text = std::string(1, symbol);
-                font->draw({to.x + ((position.x - min.x) * font->getFrameWidth()), to.y + ((position.y - min.y) * font->getFrameHeight())}, text, renderer);
-            }
-
-            position.x += 1;
-        }
-
-        position.x = 0;
-        position.y += 1;
-    }
-
-    font->setColor(&theme->cursor);
-    font->draw({to.x + ((cursor.x - min.x) * font->getFrameWidth()), to.y + ((cursor.y - min.y) * font->getFrameHeight())}, "_", renderer);
-}
+void applicationLoop(SDL_Window *window, SDL_Renderer *renderer, int argc, char **argv);
+void drawDocument(Document document, SDL_Point to, SDL_Point bounds, color_theme_t *theme, FontSheet *font, SDL_Renderer *renderer);
+std::string readFile(std::string path);
+bool writeFile(std::string path, std::string text);
 
 int main(int argc, char **argv)
 {
@@ -344,8 +170,7 @@ void applicationLoop(SDL_Window *window, SDL_Renderer *renderer, int argc, char 
 
             // Draw text editor
             to = {0, 24};
-            SDL_Point cursor = {document.getCursorX(), document.getCursorY()};
-            draw(document.getLines(), to, cursor, bounds, &theme, &font, renderer);
+            drawDocument(document, to, bounds, &theme, &font, renderer);
 
             // Draw status bar
             for (auto width = 0; width < 640; width += 16)
@@ -543,5 +368,183 @@ void applicationLoop(SDL_Window *window, SDL_Renderer *renderer, int argc, char 
         }
 
         lastUpdate = SDL_GetTicks();
+    }
+}
+
+void drawDocument(Document document, SDL_Point to, SDL_Point bounds, color_theme_t *theme, FontSheet *font, SDL_Renderer *renderer)
+{
+    std::vector<std::string> lines = document.getLines();
+    SDL_Point cursor = {document.getCursorX(), document.getCursorY()};
+    SDL_Point position = {0, 0};
+    SDL_Point min = {(cursor.x / bounds.x) * bounds.x, (cursor.y / bounds.y) * bounds.y};
+    SDL_Point max = {min.x + bounds.x, min.y + bounds.y};
+    auto escaping = false;
+    enum stream_state_t {
+        NOTHING_STREAM_STATE,
+        COMMENT_STREAM_STATE,
+        NUMBER_STREAM_STATE,
+        STRING_STREAM_STATE
+    };
+    auto state = NOTHING_STREAM_STATE;
+
+    for (auto line : lines)
+    {
+        for (auto symbol : line)
+        {
+            if (escaping)
+            {
+                escaping = false;
+            }
+            else
+            {
+                switch (symbol)
+                {
+                    case SYMBOL_ESCAPE:
+                        escaping = true;
+                        break;
+                    case SYMBOL_COMMENT:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            state = COMMENT_STREAM_STATE;
+                        }
+                        else if (state == COMMENT_STREAM_STATE)
+                        {
+                            state = NOTHING_STREAM_STATE;
+                        }
+
+                        break;
+                    case SYMBOL_NUMBER:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            state = NUMBER_STREAM_STATE;
+                        }
+                        else if (state == NUMBER_STREAM_STATE)
+                        {
+                            state = NOTHING_STREAM_STATE;
+                        }
+
+                        break;
+                    case SYMBOL_STRING:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            state = STRING_STREAM_STATE;
+                        }
+                        else if (state == STRING_STREAM_STATE)
+                        {
+                            state = NOTHING_STREAM_STATE;
+                        }
+
+                        break;
+                }
+
+                switch (symbol)
+                {
+                    case SYMBOL_ESCAPE:
+                        break;
+                    case SYMBOL_COMMENT:
+                        if (state == COMMENT_STREAM_STATE)
+                        {
+                            font->setColor(&theme->comments);
+                        }
+
+                        break;
+                    case SYMBOL_NUMBER:
+                        if (state == NUMBER_STREAM_STATE)
+                        {
+                            font->setColor(&theme->numbers);
+                        }
+
+                        break;
+                    case SYMBOL_STRING:
+                        if (state == STRING_STREAM_STATE)
+                        {
+                            font->setColor(&theme->strings);
+                        }
+
+                        break;
+                    case SYMBOL_LIST_START:
+                    case SYMBOL_LIST_END:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            font->setColor(&theme->lists);
+                        }
+
+                        break;
+                    case SYMBOL_MAP_START:
+                    case SYMBOL_MAP_END:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            font->setColor(&theme->maps);
+                        }
+
+                        break;
+                    case SYMBOL_CALL_START:
+                    case SYMBOL_CALL_END:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            font->setColor(&theme->calls);
+                        }
+
+                        break;
+                    case SYMBOL_NULL:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            font->setColor(&theme->nulls);
+                        }
+
+                        break;
+                    default:
+                        if (state == NOTHING_STREAM_STATE)
+                        {
+                            font->setColor(&theme->undefined);
+                        }
+
+                        break;
+                }
+            }
+
+            if (position.x >= min.x && position.x < max.x && position.y >= min.y && position.y < max.y)
+            {
+                auto text = std::string(1, symbol);
+                font->draw({to.x + ((position.x - min.x) * font->getFrameWidth()), to.y + ((position.y - min.y) * font->getFrameHeight())}, text, renderer);
+            }
+
+            position.x += 1;
+        }
+
+        position.x = 0;
+        position.y += 1;
+    }
+
+    font->setColor(&theme->cursor);
+    font->draw({to.x + ((cursor.x - min.x) * font->getFrameWidth()), to.y + ((cursor.y - min.y) * font->getFrameHeight())}, "_", renderer);
+}
+
+std::string readFile(std::string path)
+{
+    std::ifstream stream(path);
+
+    if (stream.is_open())
+    {
+        return std::string((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+    }
+    else
+    {
+        return "`Unable to load file.`";
+    }
+}
+
+bool writeFile(std::string path, std::string text)
+{
+    std::ofstream stream(path);
+
+    if (stream.is_open())
+    {
+        stream << text;
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
