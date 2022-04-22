c547a1e3871e0ab95b83780fe720e9ab0294c741
pubcontrib <pub.contrib@protonmail.com>
2022-04-21 20:42:43 -0500

    Added the ability to select a font style with a new inverted option.

--- src/FontSheet.hpp
+++ src/FontSheet.hpp
@@ -6,15 +6,23 @@
 #include <SDL2/SDL.h>
 #include <string>
 
+enum class FontStyle : int
+{
+    BASIC = 0,
+    INVERTED = 1
+};
+
 class FontSheet
 {
     SpriteSheet *sheet;
+    FontStyle style;
 
     public:
         FontSheet(SpriteSheet *sheet);
         int getFrameWidth();
         int getFrameHeight();
         void setColor(SDL_Color *color);
+        void setStyle(FontStyle style);
         void draw(SDL_Point to, std::string text, SDL_Renderer *renderer);
 };
 
